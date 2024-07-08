58c51511bd23c6c623c2346d8d7287fb7c235953
Paulo Freire <paulgarden95@gmail.com>
2024-07-08 06:29:42 -0300

    feat: redid Weekgrid component CSS

--- src/Weekgrid.js
+++ src/Weekgrid.js
@@ -1,13 +1,20 @@
 
 const weekStyle = {
-    border: '1px solid black',
+  border: '1px solid black',
+  display: 'flex',
+  justifyContent: 'space-evenly',
+}
+
+const dayBox = {
+  border: '1px solid black',
+  width: '14.2%'
 }
 
 const Weekgrid = () => {
 
-    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     const weekDays = daysOfWeek.map((day, index) => (
-      <div key={index}>{day}</div>
+      <div style={dayBox} key={index}>{day}</div>
     ));
   
     return (
